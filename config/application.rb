fdb05af6426488df47f158de47d5ee7c4b33cc2a
hujuu <1961946+hujuu@users.noreply.github.com>
2023-02-01 16:36:30 +0900

    lib directoryの読み込み

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module RailsApiAuth0
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.auth0 = config_for(:auth0)
+    config.autoload_paths << Rails.root.join("lib")
   end
 end
