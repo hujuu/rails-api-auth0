2221c6158ea5ada26824aa8609318c9f48920a62
hujuu <1961946+hujuu@users.noreply.github.com>
2023-02-02 18:50:32 +0900

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f tmp/pids/server.pid
+mkdir -p tmp/sockets
+mkdir -p tmp/pids
+
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+exec "$@"
